959707928da9b78b1f5f79c6fe3e35cf8de36fcc
Amanda Pereira <97693534+devamandapereira@users.noreply.github.com>
2022-01-20 13:59:06 -0300

    novo projeto e conserto de erro em js

---
+++ projetos pessoais/festa.js
@@ -0,0 +1,7 @@
+let pessoa = 'Amanda' 
+
+if (pessoa == 'Amanda') {
+console.log (`Olá, ${pessoa}. Seu nome está na lista. Seja bem-vindo!`)}
+else {
+    console.log (`Olá, ${pessoa}. Seu nome não está na lista. Você não pode entrar.`)
+}
